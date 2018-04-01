29f0ee975c13d61228d7e9c7c1f0304af1715644
Agung Setiaji <mragungsetiaji@gmail.com>
2018-04-01 16:28:56 +0700

    Update interpolation.py

--- scr/interpolation.py
+++ scr/interpolation.py
@@ -1,52 +1,29 @@
+#! /usr/bin/python
 '''
     Data Interpolation
-
-    >   Cara menentukan nilai yang berada di antara dua nilai diketahui
-        berdasarkan suatu fungsi persamaan
 '''
 
 import os, sys
 import pandas as pd
 
-class Interpolation(object):
-    
-    def __init__(self, df, cols, dir_path):
-        '''
-        df
-        >   Dataframe
+def interpolate(dataframe, cols_to_interpolate):
 
-        cols
-        >   Nama column untuk interpolasi
+    for col in cols_to_interpolate:
+        dataframe[col] = dataframe[col].interpolate('spline', order=2)
 
-        dir_path
-        >   dir_path
-        '''
-        self.df = df
-        self.cols = cols
-        self.dir_path = dir_path
-
-    def interpolate(self):
-        
-        for col in self.cols:
-            df[col] = df[col].interpolate('spline', order=2)
-
-        return df 
-
-    def process(self):
-        
-        files = os.listdir(self.dir_path)
-        for file_name in files:
-            dataframe = pd.read_csv(os.path.join(self.dir_path, file_name))
-            dataframe = interpolate(dataframe, ['high', 'open', 
-                                                'low', 'close', 
-                                                'volume'. 'adj_close'])
-            print dataframe
-
-            break
+    return dataframe
 
 
+def main(dir_path):
+    files = os.listdir(dir_path)
+    for file_name in files:
+        dataframe = pd.read_csv(os.path.join(dir_path, file_name))
+        dataframe = interpolate(dataframe, \
+            ['high', 'open', 'low', 'close', 'volume', 'adj_close'])
+        print dataframe
 
-            
-            
+        break
 
 
+if __name__=="__main__":
+main(sys.argv[1])
